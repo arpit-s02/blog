3457ad3d53a8cabcbca9cea9075749234366574f
arpit-s02 <arpitrahul02@gmail.com>
2023-02-03 21:23:26 +0530

    complete the home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,39 @@
+import { collection } from 'firebase/firestore/lite';
+import { useState, useEffect } from 'react';
+import {db} from '../firebaseConfig';
+import { getDocs} from 'firebase/firestore/lite';
+import { Link } from 'react-router-dom';
+
 function Home(){
+    const [posts, setPost] = useState([]);
+
+    useEffect(() => {
+        async function getPosts() {
+            const postsCol = collection(db, 'posts');
+            const postsDocs = await getDocs(postsCol);
+            const postsList = postsDocs.docs.map((doc) => {
+                return {...doc.data(), id: doc.id}
+            })
+
+            setPost(postsList);
+        }
+
+        getPosts();
+    }, [])
+
     return(
-        <div>
-            Home
+        <div className='home'>
+            <h1> Blogs </h1>
+            <div id = "blog-by"> Arpit </div>
+            {posts.map((post) => (
+                <div className='post' key = {post.id}>
+                    <Link to = {`/post/${post.id}`}>
+                        <h3> {post.title} </h3>
+                    </Link>
+                    <p> {post.subTitle} </p>
+                </div>
+            ))}
         </div>
-        
     );
 }
 
